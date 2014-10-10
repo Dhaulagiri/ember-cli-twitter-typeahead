4d59d3e00aa34eb9e93342d43b758ac682db042c
Bastion Fennell <bastionfennell@thefrontside.net>
2014-10-10 14:36:04 -0500

    First attempt at a full integration test suite

--- tests/integration/typeahead-test.js
+++ tests/integration/typeahead-test.js
@@ -17,3 +17,106 @@ test('shows the typeahead input box', function() {
     ok($(".tt-input").length);
   });
 });
+
+test('it gives suggestions', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+    ok($(".tt-suggestion").length === 4);
+  });
+});
+
+test('it does not give incorrect suggestions', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("test1");
+    typeahead.trigger("input");
+    ok($(".tt-suggestion").length === 2);
+  });
+});
+
+test('it displays the empty template when there are no options', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("notanoption");
+    typeahead.trigger("input");
+    ok($(".tt-suggestion").text() === "Empty");
+  });
+});
+
+test('it displays the footer at the bottom of the suggestions', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+    ok($(".tt-suggestion:last").text() === "Footer");
+  });
+});
+
+test('hitting tab selects the next response', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+    var e = $.Event('keydown');
+    e.keyCode= 9;
+    typeahead.trigger(e);
+    ok(typeahead.val() === "test1");
+  });
+});
+
+test('hitting right selects the next response', function() {
+  expect(1);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+    var e = $.Event('keydown');
+    e.keyCode= 39;
+    typeahead.trigger(e);
+    ok(typeahead.val() === "test1");
+  });
+});
+
+test('hitting down iterates through the responses', function() {
+  expect(2);
+
+  visit('/');
+
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+    var e = $.Event('keydown');
+    e.keyCode= 40;
+    typeahead.trigger(e);
+    ok(typeahead.val() === "test1");
+
+    typeahead.trigger(e);
+    ok(typeahead.val() === "test2");
+  });
+});
