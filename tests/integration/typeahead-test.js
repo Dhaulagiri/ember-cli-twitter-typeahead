a3fac7826fc8b8a393960c9a0824df7aad754e35
Bastion Fennell <bastionfennell@thefrontside.net>
2014-10-10 12:59:35 -0500

    First spike at an integration test, getting error on visit

---
+++ tests/integration/typeahead-test.js
@@ -0,0 +1,19 @@
+import startApp from "ember-cli-twitter-typeahead/tests/helpers/start-app";
+import Ember from 'ember';
+
+var App;
+
+module('integration:typeahead', {
+    setup: function() { App = startApp(); },
+    teardown: function() { Ember.run(App, App.destroy); }
+});
+
+test('shows the typeahead input box', function() {
+  expect(1);
+
+  visit("/");
+
+  andThen(function() {
+    ok($(".tt-input").length);
+  });
+});
